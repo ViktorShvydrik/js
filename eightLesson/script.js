5d429d33576a57779932f4cf923bfbda11ffce2a
Victor <mwinds45@gmail.com>
2018-06-29 16:32:00 +0300

    add eightLesson

---
+++ eightLesson/script.js
@@ -0,0 +1,10 @@
+var name = prompt('Введите выше имя.', '');
+if (name == ''){
+  alert('Вы не ввели имя');
+}else{
+if(name == 'null'){
+  alert('Вы не ввели имя');
+}else{
+  alert('Ваше имя: '+ name);
+}
+}
